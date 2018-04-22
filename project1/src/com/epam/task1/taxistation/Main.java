96888d8b8a679889e478eed3cc91f2a75e5512fd
aliaksandr-harkusha <38460516+aliaksandr-harkusha@users.noreply.github.com>
2018-04-22 11:49:53 +0300

    Update Main.java

--- project1/src/com/epam/task1/taxistation/Main.java
+++ project1/src/com/epam/task1/taxistation/Main.java
@@ -1,8 +1,10 @@
+//добавь этот класс еще в пакет какой-нибудь controller или runner 
 package com.epam.task1.taxistation;
 
 import com.epam.task1.taxistation.model.TaxiStation;
 import com.epam.task1.taxistation.view.TaxiMenu;
 
+//Класс тоже лучше переименовать в Controller либо Runner либо Starter
 public class Main {
 
 	public static void main(String[] args) {
